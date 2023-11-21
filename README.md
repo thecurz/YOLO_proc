da765438d33d1ec1aa35c4c412f9a4412bde68a5
Carlos Castilla <63982489+thecurz@users.noreply.github.com>
2023-11-21 11:05:17 -0500

    readme

--- README.md
+++ README.md
@@ -4,3 +4,7 @@
 
 - Video grabado en el break
 - Se utiliza el proyecto YoloV5 (https://github.com/ultralytics/yolov5)
+
+El algoritmo YoloV5 detecta las personas que aparecen en el video. Nuestro proyecto utiliza una modificación del script detect.py para mostrar la cantidad de personas en cada frame en la consola:
+
+![Ejemplo](./Screenshot_1.jpg)
--- main.py
+++ main.py
@@ -1,17 +1,25 @@
 import os 
 import sys
 import subprocess
+from pathlib import Path
 
 if sys.version_info.major != 3 or sys.version_info.minor != 10:
     sys.exit("Este script necesita Python 3.10 para funcionar.")
 
 print(sys.version_info)
+
+# Si el directorio 'yolov5' no existe, clona el repositorio de GitHub
 if not os.path.isdir('yolov5'):
     subprocess.run(['git', 'clone', 'https://github.com/ultralytics/yolov5'], check=True)
 subprocess.run(['pip', 'install', '-r', './yolov5/requirements.txt'], check=True)
 subprocess.run(['pip', 'install', 'moviepy'], check=True)
 
 from moviepy.editor import VideoFileClip
+import shutil
+
+sys.path.append(str(Path('./yolov5').resolve()))
+shutil.copy('./detect.py', './yolov5/detect.py')
+from detect import run
 
 if not os.path.isfile('OUTPUT.mp4'):
     clip = VideoFileClip("INPUT.mp4")
@@ -19,4 +27,9 @@ if not os.path.isfile('OUTPUT.mp4'):
     clip = clip.set_duration(120).set_fps(24)
     clip.write_videofile("OUTPUT.mp4")
 
-subprocess.run(['python3.10', './yolov5/detect.py', '--weights', 'yolov5s.pt', '--source', 'OUTPUT.mp4', '--view-img', '--classes', '0', '--save-txt'], check=True)
+weights = 'yolov5s.pt'
+source = 'OUTPUT.mp4'
+view_img = True
+classes = [0]
+
+run(weights=weights, source=source, view_img=view_img, classes=classes)
